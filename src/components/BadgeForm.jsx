2e69f5184c3c385151ee9fd22374d0a2142890f1
Isaac Enriquez <isaac-e@live.com.mx>
2021-05-26 19:34:37 -0600

    adding badgeform.jsx

---
+++ src/components/BadgeForm.jsx
@@ -0,0 +1,80 @@
+import React, {Component} from "react"
+import './style/BadgeForm.css'
+
+class BadgeForm extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                <div className="BadgeForm m-5">
+                    <form onSubmit={this.props.onSubmit}>
+                        <div className="form-group mb-2">
+                            <label className="mb-1">
+                                Name
+                            </label>
+                            <input 
+                                type="text"
+                                className="form-control"
+                                name="name"
+                                onChange={this.props.onChange}
+                                value={this.props.formValues.name} 
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <label className="mb-1">
+                                Profile Picture URL
+                            </label>
+                            <input 
+                                type="text"
+                                className="form-control"
+                                name="profile_pic_url"
+                                onChange={this.props.onChange}
+                                value={this.props.formValues.profile_picture} 
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <label className="mb-1">
+                                Badge Hero URL
+                            </label>
+                            <input 
+                                type="text"
+                                className="form-control"
+                                name="badge_hero_url"
+                                onChange={this.props.onChange}
+                                value={this.props.formValues.header_picture} 
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <label className="mb-1">
+                                Age
+                            </label>
+                            <input 
+                                type="text"
+                                className="form-control"
+                                name="age"
+                                onChange={this.props.onChange}
+                                value={this.props.formValues.age} 
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <label className="mb-1">
+                                City
+                            </label>
+                            <input 
+                                type="text"
+                                className="form-control"
+                                name="city"
+                                onChange={this.props.onChange}
+                                value={this.props.formValues.city} 
+                            />
+                        </div>
+                        <button type="submit" className="Submit__button">
+                            Save
+                        </button>
+                    </form>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default BadgeForm;
