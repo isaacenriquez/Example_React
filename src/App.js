f3143688d738931dfc5b1325562c292094193452
Isaac Enriquez <isaac-e@live.com.mx>
2021-05-26 20:42:44 -0600

    adding login path to app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom"
 import Layout from "./pages/Layout/Layout"
 import Main from "./pages/main/main"
 import NewBadge from "./pages/NewBadge/NewBadge"
+import login from "./pages/login/login"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Main}></Route>
           <Route exact path="/new" component={NewBadge}></Route>
+          <Route exact path="/login" component={login}></Route>
           <Route component={Error}></Route>
         </Switch>
       </Layout>
