0ebf40b99af2b3cf8dba32b137333bfc1845c2cd
Isaac Enriquez <isaac-e@live.com.mx>
2021-05-26 19:35:04 -0600

    adding footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import "./style/Footer.css"
+
+const Footer = () =>{
+    return(
+        <React.Fragment>
+            <div className="Footer">
+                <p>This is an example made with reactJs @utchbis 🐱‍👤</p>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default Footer;
+
