133ad8e9b927244d244a560143ff6ec4a568b6c2
Isaac Enriquez <isaac-e@live.com.mx>
2021-05-26 19:36:45 -0600

    adding mainbutton,jsx

---
+++ src/components/MainButton.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import {Link} from "react-router-dom"
+import "./style/MainButton.css"
+
+const Button = props =>{
+    return(
+        <React.Fragment>
+            <Link className="Button" to={props.link}>
+                <p>{props.contentText}</p>
+            </Link>
+        </React.Fragment>
+    );
+}
+
+export default Button
